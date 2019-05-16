2c50751bc6bb6874ee85cd5017054054cef86bf3
AseliaA <aselia.azimkanova@gmail.com>
2019-05-16 15:04:58 +0600

    1)Made a packages     + model     + repository     + service     + controller     + bootstrap     + config     + enums 2)Added a application.properties

---
+++ src/main/java/com/example/USTAT/UstatApplication.java
@@ -0,0 +1,13 @@
+package com.example.USTAT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UstatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UstatApplication.class, args);
+	}
+
+}
