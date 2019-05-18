214d5dd8dc41000b6e74d4dd77ab537f34664d59
AseliaA <aselia.azimkanova@gmail.com>
2019-05-18 18:04:37 +0600

    1) Made a enum "IT Development"

---
+++ src/main/java/com/example/USTAT/enums/ITDevelopment.java
@@ -0,0 +1,13 @@
+package com.example.USTAT.enums;
+
+public enum ITDevelopment {
+    Java,
+    Python,
+    HTMLAndCSS,
+    JavaScript,
+    PHP,
+    Android,
+    IOS;
+
+    ITDevelopment(){}
+}
