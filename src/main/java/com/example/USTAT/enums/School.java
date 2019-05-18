e320fc38f47f90bd3b043e1445ee66aa0eedf081
AseliaA <aselia.azimkanova@gmail.com>
2019-05-18 17:47:58 +0600

    1) Made a enum "School"

---
+++ src/main/java/com/example/USTAT/enums/School.java
@@ -0,0 +1,14 @@
+package com.example.USTAT.enums;
+
+public enum School {
+    EnglishLanguage,
+    Algebra,
+    Math,
+    RussianLanguage,
+    Biology,
+    Geographic,
+    PreschoolEducation,
+    Geometry;
+
+    School(){}
+}
