3ef1707cb224f831c2d808a618ce000945b9d89e
AseliaA <aselia.azimkanova@gmail.com>
2019-05-21 20:25:11 +0600

    1) Made a enum "Location"

---
+++ src/main/java/com/example/USTAT/enums/Location.java
@@ -0,0 +1,16 @@
+package com.example.USTAT.enums;
+
+public enum Location {
+    Bishkek,
+    Chuy,
+    Osh,
+    Issyk_Kul,
+    Naryn,
+    Jalal_Abad,
+    Batken,
+    Talas;
+
+
+    Location(){}
+}
+
