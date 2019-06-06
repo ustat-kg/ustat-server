a4f4011e5a3ccc87a66171dcf7d55b12b2cd75f6
AseliaA <aselia.azimkanova@gmail.com>
2019-06-06 13:55:18 +0600

    Returning response in Location Controller

--- src/main/java/com/example/USTAT/controller/LocationController.java
+++ src/main/java/com/example/USTAT/controller/LocationController.java
@@ -1,6 +1,7 @@
 package com.example.USTAT.controller;
 
 import com.example.USTAT.model.Location;
+import com.example.USTAT.model.Response;
 import com.example.USTAT.service.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -19,26 +20,27 @@ public class LocationController {
 
     @GetMapping(path = "/getLocation/{id}" ,
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public Location getLocationById(@PathVariable Long id){
-        return this.locationService.getLocationById(id);
+    public Response getLocationById(@PathVariable Long id){
+        return new Response(true,"Getting location by id",this.locationService.getLocationById(id));
     }
 
     @GetMapping(path = "/getAllLocations",
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public List<Location> getAllLocations(){
-        return this.locationService.getAllLocations();
+    public Response getAllLocations(){
+        return new Response(true,"Getting all locations" ,this.locationService.getAllLocations());
     }
 
     @PostMapping(path = "/addLocation" ,
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public Location addLocation(@RequestBody Location location){
-        return this.locationService.addLocation(location);
+    public Response addLocation(@RequestBody Location location){
+        return new Response(true,"Adding location",this.locationService.addLocation(location)) ;
     }
 
 
     @DeleteMapping(path = "/admin/deleteLocation/{id}" ,
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public void deleteLocationById(@PathVariable Long id){
+    public Response deleteLocationById(@PathVariable Long id){
         this.locationService.deleteLocationById(id);
+        return new Response(true,"Deleting location by id",null);
     }
 }
