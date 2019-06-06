2bebbaacf7899e455315c71b5f6a6e82ddc8ed39
AseliaA <aselia.azimkanova@gmail.com>
2019-06-06 14:58:10 +0600

    Tried to do getTeachersByFirstRequest

--- src/main/java/com/example/USTAT/repository/TeacherRepository.java
+++ src/main/java/com/example/USTAT/repository/TeacherRepository.java
@@ -7,4 +7,10 @@ import org.springframework.stereotype.Repository;
 @Repository("teacherRepository")
 public interface TeacherRepository
         extends JpaRepository<Teacher,Long> {
+//
+//    @Query("select p from Payment p join fetch p.client c " +
+//            "where c.phoneNo = coalesce(:phoneNo, c.phoneNo) " +
+//            "and c.fio = coalesce(:fio, c.fio) " +
+//            "and p.status = coalesce(:status, p.status) " +
+//            "")
 }
